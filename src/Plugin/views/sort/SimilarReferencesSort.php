f39342b2208b779d389f98e3088171d6f56943fd
Eric Pugh <pugh.eric@gmail.com>
2017-02-16 14:58:41 -0500

    Added sort, field, and argument files

---
+++ src/Plugin/views/sort/SimilarReferencesSort.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\similarreferences\Plugin\views\sort;
+
+use Drupal\views\Plugin\views\sort\SortPluginBase;
+
+/**
+ * Handler which sort by the similarity.
+ *
+ * @ingroup views_sort_handlers
+ *
+ * @ViewsSort("similar_references_sort")
+ */
+class SimilarReferencesSort extends SortPluginBase {
+
+  /**
+   * Define default sorting order.
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['order'] = array('default' => 'DESC');
+    return $options;
+  }
+
+  /**
+   * Add orderBy.
+   */
+  public function query() {
+    $this->ensureMyTable();
+    $this->query->addOrderBy($this->tableAlias, 'nid', $this->options['order'], NULL, array('function' => 'count'));
+  }
+
+}
